20fb94d30bbd6f6c06da8a921aa33be9ea91c154
MainPC <129085079+808Jade@users.noreply.github.com>
2023-12-14 01:49:10 +0900

    vertex 설명 update

--- CG_termproject/vertex.glsl
+++ CG_termproject/vertex.glsl
@@ -3,15 +3,15 @@
 layout (location = 0) in vec3 vPos;
 layout (location = 1) in vec3 vNormal;
 
-out vec3 FragPos;
-out vec3 Normal;
+out vec3 FragPos;		// 객체의 위치값을 프래그먼트 셰이더로 보낸다.
+out vec3 Normal;		// 노멀값을 프래그먼트 셰이더로 보낸다.
 
 uniform vec3 inColor;
 out vec3 outColor;
 
-uniform mat4 transform;
-uniform mat4 projection;
-uniform mat4 view;
+uniform mat4 transform;		// 모델링 변환값
+uniform mat4 projection;	// 투영 변환값
+uniform mat4 view;			// 뷰잉 변환값
 
 void main(void) 
 {
