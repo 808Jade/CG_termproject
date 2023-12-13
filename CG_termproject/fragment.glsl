0e2b1daeeed8c6f6ceeffbaa0a02fcf8ec20189c
MainPC <129085079+808Jade@users.noreply.github.com>
2023-12-14 01:47:40 +0900

    fragment 설명 업데이트

--- CG_termproject/fragment.glsl
+++ CG_termproject/fragment.glsl
@@ -10,25 +10,26 @@ uniform vec3 viewPos;
 
 void main()
 {
-	vec3 ambientLight = vec3(0.3);
-	vec3 ambient = ambientLight * lightColor;
-	vec3 normalVector = normalize(Normal);
+	vec3 ambientLight = vec3(0.3);								// 주변 조명 세기
+	vec3 ambient = ambientLight * lightColor;					// 주변 조명 값
+
+	vec3 normalVector = normalize(Normal);				
 	vec3 lightDir = normalize(lightPos - FragPos);
-
-	int shininess = 128;
-	vec3 viewDir = normalize(viewPos - FragPos);
-	vec3 reflectDir = reflect(-lightDir, normalVector);
-	float specularLight = max(dot(viewDir, reflectDir), 0.0);
-	specularLight = pow(specularLight, shininess);
-	vec3 specular = specularLight * lightColor;
-
-	float diffuseLight = max(dot(normalVector, lightDir), 0.0);
-
+	float diffuseLight = max(dot(normalVector, lightDir), 0.0);	// N과 L의 내적 값으로 강도 조절 : 음수 방지
+	vec3 diffuse = diffuseLight * lightColor;					// 산란 반사 조명값 : 산란반사값 * 조명색상값
 	// vec4 diffuse = diffuseLight * lightColor * objectColor;
-	vec3 diffuse = diffuseLight * lightColor;
+	
+	int shininess = 128;										// 광택 계수
+	vec3 viewDir = normalize(viewPos - FragPos);				// 관찰자의 방향
+	vec3 reflectDir = reflect(-lightDir, normalVector);			// 반사방향 : reflect 함수 - 입사 벡터의 반사 방향 계산
+	float specularLight = max(dot(viewDir, reflectDir), 0.0);	// V R 내적값으로 강도조절 : 음수 방지
+	specularLight = pow(specularLight, shininess);				// 하이라이트 만들어주기
+	vec3 specular = specularLight * lightColor;					// 거울 반사 조명값 : 거울반사값 * 조명 색상값
 
-	vec3 result = (ambient + diffuse + specular) * objectColor;
 
 
-	FragColor = vec4(result, 1.0);
+	vec3 result = (ambient + diffuse + specular) * objectColor;	// 최종 조명 설정된 픽셀 색상 : (주변 + 산란반사 + 거울반사 조명) * 객체 색상
+
+
+	FragColor = vec4(result, 1.0);								// 픽셀 색을 출력
 }
